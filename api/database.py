e8242f2a4558adda84a342fbfc5e0bc50ce27994
justnoted <just.noted3505@gmail.com>
2025-02-04 03:02:34 -0600

    Updated main.py, database.py expense methods

--- api/database.py
+++ api/database.py
@@ -1,5 +1,6 @@
 import os
 import pymongo
+from bson import ObjectId
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,13 +10,34 @@ db = client["ExpenseTrackerAPI"]
 expenses = db.get_collection("expenses")
 debts = db.get_collection("debts")
 
-def get_items(items):
-    for item in items:
-        item['_id'] = str(item['_id'])
-    return items
-
 def get_all_expenses():
-    return list(expenses.find())
+    return [convert_id_to_str(expense) for expense in list(expenses.find())]
+
+def get_expense_with_id(expense_id):
+    doc = expenses.find_one({"_id": ObjectId(expense_id)})
+    return None if not doc else convert_id_to_str(doc)
+
+def create_expense(expense):
+    exp = expense.model_dump()
+    result = expenses.insert_one(exp)
+
+    return result.inserted_id if result.inserted_id else None
+
+def update_expense(expense_id, expense):
+    exp = expense.model_dump()
+    result = expenses.find_one_and_update({"_id": ObjectId(expense_id)}, {"$set": exp})
+    print(result)
+    return result
+
+def delete_expense(expense_id):
+    result = expenses.find_one_and_delete({"_id": ObjectId(expense_id)})
+    return result
+
+def convert_id_to_str(item):
+    item['_id'] = str(item['_id'])
+    return item
+
+# Debts
 
 def get_all_debts():
     return list(debts.find())
--- api/main.py
+++ api/main.py
@@ -1,18 +1,68 @@
-from fastapi import FastAPI
+from bson import ObjectId
+from fastapi import FastAPI, HTTPException
+from starlette import status
+from datetime import datetime
 import database as db
+from models import *
 
 app = FastAPI()     # Run "fastapi run main.py" to use with Expenses App
 
 @app.get('/')
 def index():
-    return {"status": "what?"}
+    return {"status": "hmm?"}
 
 @app.get('/expenses')
 def get_all_expenses():
-    return db.get_items(db.get_all_expenses())
+    return db.get_all_expenses()
+
+@app.get("/expenses/{expense_id}")
+def get_expense_by_id(expense_id: str):
+    if not ObjectId.is_valid(expense_id):
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid ObjectId")
+
+    expense = db.get_expense_with_id(expense_id)
+
+    if not expense:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense not found")
+    return expense
+
+@app.post("/expenses", status_code=status.HTTP_201_CREATED)
+def add_expense(expense: Expense):
+    expense.date = datetime.now().isoformat()
+    result = db.create_expense(expense)
+
+    if not result:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Unable to create expense")
+
+    return expense
+
+@app.put("/expenses/{expense_id}")
+def edit_expense(expense_id: str, expense: Expense):
+    if not ObjectId.is_valid(expense_id):
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid ObjectId")
+
+    result = db.update_expense(expense_id, expense)
+
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No expense was found to update")
+
+    return {"status": "Updated successfully"}
+
+@app.delete("/expenses/{expense_id}")
+def delete_expense(expense_id: str):
+    if not ObjectId.is_valid(expense_id):
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid ObjectId")
+
+    result = db.delete_expense(expense_id)
+
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No expense was found to delete")
+
+    return {"status": "Deleted successfully"}
+
 
 @app.get('/debts')
-def get_all_expenses():
+def get_all_debts():
     debts = db.get_all_debts()
     for debt in debts:
         debt['_id'] = str(debt['_id'])
